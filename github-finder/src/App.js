7a0c27111d8a032aed22f991d3cb84565be7b33b
Alphacode18 <skharban@purdue.edu>
2020-07-18 09:13:14 +0530

    Components, Props, and PropTypes

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Shreyas Kharbanda';
-    const loading = false;
     return (
       <div className="app">
-        <h1>My App</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello from {name}</h1>}
+        <Navbar />
       </div>
     );
   }
