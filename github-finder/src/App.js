6b73e6cb0d89d49c3b01242a8b4965fbb7f36249
Alphacode18 <skharban@purdue.edu>
2020-07-20 07:30:50 +0530

    Base Page Completed

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+  
+  async componentDidMount() {
+    this.setState( {loading: true} );
+    const response = await axios.get('https://api.github.com/users?since=135');
+    this.setState( {users: response.data, loading: false});
+  }
+
   render() {
     return (
       <div className="app">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users}/>
         </div>
       </div>
     );
