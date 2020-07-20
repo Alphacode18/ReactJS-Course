05d5ec34a285d316dd2eb40e1ced29f31fb6cb4d
Alphacode18 <skharban@purdue.edu>
2020-07-20 07:42:33 +0530

    API Endpoint Added

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
   
   async componentDidMount() {
     this.setState( {loading: true} );
-    const response = await axios.get('https://api.github.com/users?since=135');
+    const response = await axios.get(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENTSECRET}`);
     this.setState( {users: response.data, loading: false});
   }
 
