df31b9bf85ff31258fc918df439a559d78fd837c
Alphacode18 <skharban@purdue.edu>
2020-07-28 13:58:10 +0530

    Context API

---
+++ github-finder/src/context/github/GithubState.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USERS,
+  GET_REPOS,
+  SET_LOADING,
+} from '../types';
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: '',
+    repos: [],
+    loading: false,
+  };
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+};
